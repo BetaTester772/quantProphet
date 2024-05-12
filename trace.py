13d3553faf43a12188724f342daa4a19a005667b
Hoseong Ahn <hoseong8115@naver.com>
2024-05-12 19:52:43 +0900

    원격 실행을 위한 잔고 추적(trace.py) 추가

---
+++ trace.py
@@ -0,0 +1,20 @@
+from sparklestock import Stock
+
+
+def init_stock_system() -> Stock:
+    system = Stock("http://3.34.181.14")
+    system.login("id", "pw")
+
+    print("Init Complete")
+    print(system.check_my_asset())
+
+    return system
+
+
+if __name__ == '__main__':
+    stock_system = init_stock_system()
+    while True:
+        temp = stock_system.check_my_asset()
+        if stock_system.check_my_asset() != temp:
+            temp = stock_system.check_my_asset()
+            print(temp)
