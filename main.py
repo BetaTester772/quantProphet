26bf8dbf0dfdc50a912f19a423740340644ea422
Hoseong Ahn <hoseong8115@naver.com>
2024-05-12 19:21:14 +0900

    주식 거레 시스템 종합

---
+++ main.py
@@ -0,0 +1,74 @@
+from model import StockTrader
+from sparklestock import Stock
+
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+def init_stock_system() -> Stock:
+    system = Stock("http://3.34.181.14")
+    system.login("betatester772", "thisismypassword")
+
+    print("Init Complete")
+    print(system.check_my_asset())
+
+    return system
+
+
+def get_formated_history(stock_system: Stock):
+    history: list = stock_system.get_price_history()
+
+    # 시작 날짜 설정
+    start_date = datetime.now().date() - timedelta(days=len(history))
+
+    # 날짜 인덱스 생성
+    dates = [start_date + timedelta(days=i) for i in range(len(history))]
+
+    # DataFrame 생성
+    df = pd.DataFrame(data={'Date': dates, 'Close': history})
+
+    return df
+
+
+def get_initial_stock_trader(stock_system: Stock, target_return=1.05, periods=5):
+    df = get_formated_history(stock_system)
+
+    trader = StockTrader(stock_system=stock_system, initial_data=df,
+                         capital=stock_system.check_my_asset().get('money'),
+                         target_return=target_return, periods=periods)
+
+    return trader
+
+
+def main():
+    stock_system = init_stock_system()
+    trader = get_initial_stock_trader(stock_system, target_return=1.01, periods=5)
+
+    i_date = datetime.now().date()
+
+    current_history = pd.DataFrame(data={'Date': [], 'Close': []})
+    history_len = len(stock_system.get_price_history())
+
+    while True:
+        try:
+            while True:
+                if len(stock_system.get_price_history()) > history_len + len(current_history):
+                    current_price = stock_system.get_current_price()
+                    break
+            current_history.loc[i_date] = current_price
+            trader.trade(i_date, current_price)
+            res = trader.check_model()
+            if res == "restart":
+                print("!!Alert: restarting model!!")
+                trader = get_initial_stock_trader(stock_system)
+            trader.update_model(current_history)
+            i_date += timedelta(days=1)
+        # except Exception as e:
+        #     stock_system.sell_stock(trader.stocks_owned)
+        finally:
+            pass
+            # trader.summarize_trading(last_price=current_history.iloc[-1]['Close'])
+
+
+if __name__ == '__main__':
+    main()
--- model.py
+++ model.py
@@ -2,6 +2,9 @@ import yfinance as yf
 from prophet import Prophet
 import pandas as pd
 import matplotlib.pyplot as plt
+from sparklestock import Stock
+
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 # 모델 설정 및 훈련
@@ -12,7 +15,7 @@ def train_model(data):
     model = Prophet(yearly_seasonality=False,
                     weekly_seasonality=False,
                     daily_seasonality=False,
-                    changepoint_prior_scale=0.65,
+                    changepoint_prior_scale=0.75,
                     interval_width=0.95,
                     changepoint_range=0.95)
     model.add_country_holidays(country_name='KOR')
@@ -23,22 +26,94 @@ def train_model(data):
 
 # 주식을 사고 팔기 위한 클래스
 class StockTrader:
-    def __init__(self, initial_data, capital, target_return=1.05, periods=5):
+    def __init__(self, initial_data, capital, target_return=1.05, periods=5, stock_system: Stock = None):
+        self.stock_system: Stock = stock_system
+        if stock_system is not None:
+            self.capital = stock_system.check_my_asset().get('money')
+            self.stocks_owned = stock_system.check_my_asset().get('stock')
+        else:
+            self.capital = capital
+            self.stocks_owned = 0
         self.data = initial_data.copy()
         self.model = train_model(self.data)
-        self.capital = capital
-        self.stocks_owned = 0
         self.purchase_price = 0
         self.target_return = target_return
         self.periods = periods
         self.transactions = []  # 거래 내역 저장
+        self.test_data = []
 
     def update_model(self, new_data):
         self.data = pd.concat([self.data, new_data])
         self.data.drop_duplicates(subset='Date', keep='last', inplace=True)
         self.model = train_model(self.data)
 
+    def check_model(self):
+        if len(self.test_data) >= 5:
+            cos_sim = cosine_similarity([self.test_data], [self.data['Close'].values[-5:]])
+            # print(self.test_data)
+            # print(self.data['Close'].values[-5:])
+            # print(cos_sim)
+            if cos_sim < 0.95:
+                return "restart"
+
+    def trade(self, current_date, real_price):
+        if self.stock_system is None:
+            raise ValueError("Stock System is not initialized")
+
+        future = self.model.make_future_dataframe(periods=self.periods)
+        forecast = self.model.predict(future)
+        predicted_price = forecast.iloc[-1]['yhat']  # 예측 일수 후의 예상 가격
+
+        if len(self.test_data) >= 5:
+            self.test_data.pop(0)
+        self.test_data.append(predicted_price)
+
+        # 예상 수익률 계산
+        future_return = ((predicted_price / real_price) - 1) * 100
+
+        # 현재 가격, 구매 가격 및 수익률 출력
+        current_return = (real_price / self.purchase_price - 1) * 100 if self.stocks_owned > 0 else 0
+        print(
+                f"{self.periods}일 후의 예상 가격: {predicted_price}, 예상 수익률: {future_return:.2f}%, 현재 실제 가격: {real_price}, 구매 가격: {self.purchase_price}, 현재 수익률: {current_return:.2f}%")
+
+        # 주식 추가 구매 결정: 목표 수익률 달성 가능성 판단
+        if predicted_price > real_price * self.target_return:
+            desired_quantity = int(self.capital / real_price)
+
+            # min_predicted_price = min(predicted_prices)
+
+            if desired_quantity > 0:  # and min_predicted_price >= real_price:
+                self.stocks_owned += desired_quantity
+                self.capital -= desired_quantity * real_price
+                self.purchase_price = ((self.purchase_price * (
+                        self.stocks_owned - desired_quantity) + real_price * desired_quantity) / self.stocks_owned) if self.stocks_owned else real_price
+                self.transactions.append((current_date, 'buy', desired_quantity, real_price))
+                self.stock_system.buy_stock(desired_quantity)
+                print(f"주식 구매 완료: {desired_quantity} 주")
+
+        # 주식 판매 결정 (목표 수익률 달성 시)
+        if self.stocks_owned > 0 and real_price >= self.purchase_price * self.target_return:
+            self.capital += self.stocks_owned * real_price
+            self.transactions.append((current_date, 'sell', self.stocks_owned, real_price))
+            self.stock_system.sell_stock(self.stocks_owned)
+            print(
+                    f"주식 판매 완료: 구매가 {self.purchase_price}, 판매가 {real_price}, 수힉 {(real_price - self.purchase_price) * self.stocks_owned} ,수익률 {((real_price / self.purchase_price) - 1) * 100:.2f}%")
+            self.stocks_owned = 0
+            self.purchase_price = 0
+        # 주식 판매 결정 (손실률 5% 이상)
+        if self.stocks_owned > 0 and real_price <= self.purchase_price * 0.95:
+            self.capital += self.stocks_owned * real_price
+            self.transactions.append((current_date, 'sell', self.stocks_owned, real_price))
+            self.stock_system.sell_stock(self.stocks_owned)
+            print(
+                    f"주식 판매 완료: 구매가 {self.purchase_price}, 판매가 {real_price}, 손실 {(real_price - self.purchase_price) * self.stocks_owned}, 손실률 {((real_price / self.purchase_price) - 1) * 100:.2f}%")
+            self.stocks_owned = 0
+            self.purchase_price = 0
+
     def simulate_trading(self, current_date, real_price):
+        if self.stock_system is not None:
+            raise ValueError("Stock System is initialized, unable to simulate trading")
+
         future = self.model.make_future_dataframe(periods=self.periods)
         forecast = self.model.predict(future)
         predicted_price = forecast.iloc[-1]['yhat']  # 예측 일수 후의 예상 가격
@@ -104,28 +179,29 @@ class StockTrader:
         plt.show()
 
 
-# Initialize and run the simulation
-initial_data = yf.download('005930.KS', start='2010-01-01', end='2023-12-31')
-initial_df = initial_data.reset_index()[['Date', 'Close']]
+if __name__ == '__main__':
+    # Initialize and run the simulation
+    initial_data = yf.download('005930.KS', start='2010-01-01', end='2023-12-31')
+    initial_df = initial_data.reset_index()[['Date', 'Close']]
 
-capital = 1000000  # 예시 초기 자본금: 1,000,000 KRW
-trader = StockTrader(initial_df, capital, target_return=1.01, periods=5)
-print("Init Finished")
+    capital = 1000000  # 예시 초기 자본금: 1,000,000 KRW
+    trader = StockTrader(initial_df, capital, target_return=1.01, periods=5)
+    print("Init Finished")
 
-real_data = yf.download('005930.KS', start='2024-01-01', end='2024-04-30')
-real_df = real_data.reset_index()[['Date', 'Close']]
+    real_data = yf.download('005930.KS', start='2024-01-01', end='2024-04-30')
+    real_df = real_data.reset_index()[['Date', 'Close']]
 
-import time
+    import time
 
-times = []
+    times = []
 
-# Execute the trading simulation
-for index, row in real_df.iterrows():
-    start_time = time.time()
-    trader.update_model(real_df.iloc[:index])  # Updating the model with up-to-date data
-    trader.simulate_trading(row['Date'], row['Close'])  # Pass both the date and the actual price for simulation
-    times.append(time.time() - start_time)
+    # Execute the trading simulation
+    for index, row in real_df.iterrows():
+        start_time = time.time()
+        trader.update_model(real_df.iloc[:index])  # Updating the model with up-to-date data
+        trader.simulate_trading(row['Date'], row['Close'])  # Pass both the date and the actual price for simulation
+        times.append(time.time() - start_time)
 
-# Summarize and visualize the trading performance at the end
-trader.summarize_trading(last_price=real_df.iloc[-1]['Close'])
-print("Average time per iteration: ", sum(times) / len(times))
+    # Summarize and visualize the trading performance at the end
+    trader.summarize_trading(last_price=real_df.iloc[-1]['Close'])
+    print("Average time per iteration: ", sum(times) / len(times))
